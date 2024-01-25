1241c6c2559cd6fb0fec29ad2d7a0850d46591f5
rafae <rafae@DESKTOP-PCAJLHP>
2024-01-25 21:26:07 +0100

    CrearSpringBoot Inicial

---
+++ ProjectoFinal/src/main/java/com/Mavel/ProjectoFinal/ProjectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.Mavel.ProjectoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoFinalApplication.class, args);
+	}
+
+}
---
+++ ProjectoFinal/src/test/java/com/Mavel/ProjectoFinal/ProjectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mavel.ProjectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
